8cf63f8b1578e9ae168a5f30c853ba301bbecd6c
billoreankit66@gmail.com <billoreankit66@gmail.com>
2025-01-28 23:26:31 +0530

    update

--- ReactFirstTask/src/components/InvensionStdio.jsx
+++ ReactFirstTask/src/components/InvensionStdio.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import Button from './Button'
+// import React from 'react'
+// import Button from './Button'
 
-const InvensionStdio = () => {
-  return (
-    <div className='h-10 w-full bg-pink-200 flex items-center justify-between '>
-      <h1>Invension Stdio</h1>
-      <Button text='Marketing' style='px-5 py-2 border-1 rounded-full'/>
-    </div>
-  )
-}
+// const InvensionStdio = () => {
+//   return (
+   
+//   )
+// }
 
-export default InvensionStdio
+// export default InvensionStdio
--- ReactFirstTask/src/components/Marketing.jsx
+++ ReactFirstTask/src/components/Marketing.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
-import InvensionStdio from './InvensionStdio'
+import React from "react";
+// import InvensionStdio from './InvensionStdio'
 
 const Marketing = () => {
-  return (
-    <div className='h-[70vh] w-[45vw] bg-green-600 flex flex-col gap-2'>
-       <img className='h-[90%] w-full bg-amber-900 ' src="/images/MarketImg1.webp" alt="" />
-        <InvensionStdio/>
-    </div>
-  )
-}
+  const data = [
+    {
+      img: "/images/MarketImg1.webp",
+      h1: "Invision Studio",
+      Button: "Marketing",
+    },
+    { img: "/images/MarketImg2.webp", h1: "Square Media", Button: "Branding" },
+    {
+      img: "/images/MarketImg3.webp",
+      h1: "Vision Design",
+      Button: "Social Media",
+    },
+    { img: "/images/MarketImg4.webp", h1: "Design Bros", Button: "Branding" },
+  ];
 
-export default Marketing
+  return (
+    <div className="flex flex-wrap  gap-10 p-4">
+      {data.map((item) => {
+        return (
+          <div className="h-[70vh] w-[45vw]  flex flex-col gap-2">
+            <img
+              className="h-[90%] w-full bg-amber-900 "
+              src={`${item.img}`}
+              alt=""
+            />
+            <div className="h-10 w-full  flex items-center justify-between p-4 ">
+              <h1>{item.h1}</h1>
+              <h3 className="px-4 py-1 border-1 border-solid border-black rounded-[40px]">
+                {item.Button}
+              </h3>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Marketing;
