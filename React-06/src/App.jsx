2ad3361a8885caed99c7caa07c765677353c5230
billoreankit66@gmail.com <billoreankit66@gmail.com>
2025-02-13 23:16:03 +0530

    New Update

--- React-06/src/App.jsx
+++ React-06/src/App.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+const [Val, setVal] = useState("")
 const Handeler = (e)=>{
-  console.log("helooo",e);
   e.preventDefault()
   
 }
-
   return (
     <div>
       <form className='m-12' onSubmit={(e)=>{
-        Handeler(e);
+              Handeler(e)
+              console.log(Val);
+              setVal("")
       }}>
-        <input className='border-2' type="text" placeholder="Enter your name" />
+        <input onChange={(e)=>{
+        setVal(e.target.value) 
+      }}
+        value={Val}
+          className='border-2'
+          type="text"
+          placeholder="Enter your name" 
+          />
         <button className='bg-blue-400 px-4 ml-4' type="submit">Submit</button>
       </form>
     </div>
