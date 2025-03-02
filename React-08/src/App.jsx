dc3303888687edc2481f02920b2605ebcdaaedd4
billoreankit66@gmail.com <billoreankit66@gmail.com>
2025-03-02 10:16:11 +0530

    Dynamic Routing..

--- React-08/src/App.jsx
+++ React-08/src/App.jsx
@@ -4,6 +4,10 @@ import { Route, Routes } from 'react-router-dom'
 import About from './pages/About'
 import Courses from './pages/Courses'
 import Nav from './components/Nav'
+import Error from './pages/Error'
+import HtmlCourse from './pages/HtmlCourse'
+
+
 
 const App = () => {
   return (
@@ -13,6 +17,8 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About/>} />
         <Route path="/courses" element={<Courses/>} />
+        <Route path="/courses/htmlCourses" element={<HtmlCourse/>} />
+        <Route path="/*" element={<Error/>} />
       </Routes>
     </div>
   ) 
---
+++ React-08/src/pages/HtmlCourse.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HtmlCourse = () => {
+  return (
+    <div>
+      <h1>HTML Courses...</h1>
+    </div>
+  )
+}
+
+export default HtmlCourse
