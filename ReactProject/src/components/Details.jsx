6b15328117fd7375bc1ed1c4cd3b7a78c5b34b4f
billoreankit66@gmail.com <billoreankit66@gmail.com>
2025-03-19 22:42:43 +0530

    Changes

--- ReactProject/src/components/Details.jsx
+++ ReactProject/src/components/Details.jsx
@@ -6,8 +6,8 @@ const Details = () => {
     <div className='h-full w-[70%] m-auto p-[10%] flex justify-center items-center gap-10'>
      <img className='w-[30%] h-[80%] object-cover' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="" />
         <div className='content w-[50%]'>
-            <h1 className='text-4xl'>Fjallraven-Foldsack No. 1 Backpack, Fits 15 Laptops</h1>
-            <h2 className='my-2 text-zinc-400'>men's clothing</h2>
+            <h1 className='text-4xl'>Fjallraven-Foldsack No.1 Backpack, Fits 15 Laptops</h1>
+            <h2 className='my-2 text-zinc-400'>Men's Clothing</h2>
             <h3 className='text-red-300'>$ 109.95</h3>
             <p className='text-sm mb-5'>Your perfect pack contain everyday use and walks in the forest. 
               Stash your laptop (up to 15 inches) the padded sleeve,
