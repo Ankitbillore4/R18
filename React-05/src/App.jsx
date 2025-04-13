0b7acb78cd1d78fc886f3b8f27236525047b9939
Ankit billore <163969792+Ankitbillore4@users.noreply.github.com>
2025-04-13 22:32:24 +0530

    Update App.jsx

--- React-05/src/App.jsx
+++ React-05/src/App.jsx
@@ -11,8 +11,8 @@ function App() {
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
+        </a> 
+        <a href="https://react.dev"  target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -21,7 +21,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p> 
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
